d24acecd602637de27406128b57fc81ecd24b46e
Constantine811 <constantine@ixlayer.com>
2019-10-29 21:12:27 +0200

    finished header, aside and a window profile

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,40 @@
+(() => {
+	const btnMenu = document.getElementById("btn-menu");
+	const elMenuArray = document.querySelectorAll('.menu-item');
+	const profileMenu = document.getElementById('profile-menu');
+	const btnBackDashboard = document.getElementById('btn-back-dashboard');
 
+  const initToggle = (elem) => {
+    const classList = elem.classList;
+    if (classList.contains("hidden")) {
+			classList.remove("hidden");
+			classList.add( "visible");
+    } else {
+			classList.remove("visible");
+			classList.add("hidden");
+    }
+	};
+
+
+
+	elMenuArray.forEach(function(elem) {
+		elem.addEventListener('click', function(e) {
+			initToggle(profileMenu)
+		}, false);
+	});
+
+	btnBackDashboard.addEventListener('click', e => {
+		e.preventDefault();
+		initToggle(profileMenu)
+	}, false);
+
+  btnMenu.addEventListener(
+    "click",
+    e => {
+      e.preventDefault();
+      const menu = document.getElementById("menu");
+      initToggle(menu);
+    },
+    false
+  );
+})();
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,11 +1,24 @@
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const webpack = require('webpack-stream');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function () {
   $.gulp.task('js', () => {
-    return $.gulp.src('./dev/js/**/*.js')
+    return $.gulp.src(['./dev/js/**/*.js'], {sice: $.gulp.lastRun('js')})
+      .pipe(plumber())
+      .pipe(webpack({
+        mode: 'production'
+      }))
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: ['@babel/env']
+      }))
       .pipe(concat('bundle.js'))
       .pipe(uglify())
+      .pipe(sourcemaps.write('.'))
       .pipe($.gulp.dest('./build/js/'))
       .pipe($.browserSync.reload({
         stream: true
