184bb510b8d1119e79070b83b306b5b9f62c4b2e
Constantine811 <constantine@ixlayer.com>
2019-10-30 10:37:09 +0200

    finished test

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,32 +1,39 @@
 (() => {
-	const btnMenu = document.getElementById("btn-menu");
-	const elMenuArray = document.querySelectorAll('.menu-item');
-	const profileMenu = document.getElementById('profile-menu');
-	const btnBackDashboard = document.getElementById('btn-back-dashboard');
+  const btnMenu = document.getElementById("btn-menu");
+  const elMenuArray = document.querySelectorAll(".menu-item");
+  const profileMenu = document.getElementById("profile-menu");
+  const btnBackDashboard = document.getElementById("btn-back-dashboard");
+  const dropDown = document.getElementById("drop-down");
 
-  const initToggle = (elem) => {
+  const initToggle = elem => {
     const classList = elem.classList;
     if (classList.contains("hidden")) {
-			classList.remove("hidden");
-			classList.add( "visible");
+      classList.remove("hidden");
+      classList.add("visible");
     } else {
-			classList.remove("visible");
-			classList.add("hidden");
+      classList.remove("visible");
+      classList.add("hidden");
     }
-	};
+  };
 
+  elMenuArray.forEach(function(elem) {
+    elem.addEventListener(
+      "click",
+      function(e) {
+        initToggle(profileMenu);
+      },
+      false
+    );
+  });
 
-
-	elMenuArray.forEach(function(elem) {
-		elem.addEventListener('click', function(e) {
-			initToggle(profileMenu)
-		}, false);
-	});
-
-	btnBackDashboard.addEventListener('click', e => {
-		e.preventDefault();
-		initToggle(profileMenu)
-	}, false);
+  btnBackDashboard.addEventListener(
+    "click",
+    e => {
+      e.preventDefault();
+      initToggle(profileMenu);
+    },
+    false
+  );
 
   btnMenu.addEventListener(
     "click",
@@ -37,4 +44,14 @@
     },
     false
   );
+
+  dropDown.addEventListener("click", e => {
+		e.preventDefault();
+		const thisElem = e.target;
+		if(thisElem.classList.contains('open')) {
+			thisElem.classList.remove('open');
+		} else {
+			thisElem.classList.add('open');
+		}
+	}, false);
 })();
